aa0cd4987d91c4ba531042de2ef1d23824268566
wt <1078210460@qq.com>
2024-05-11 16:44:43 +0800

    first commit

---
+++ src/static/js/tab.js
@@ -0,0 +1,70 @@
+
+/*
+ * jQuery Tools 1.2.3 - The missing UI library for the Web
+ *
+ * [tabs]
+ *
+ * NO COPYRIGHTS OR LICENSES. DO WHAT YOU LIKE.
+ *
+ * http://flowplayer.org/tools/
+ *
+ * File generated: Sat Jul 03 19:35:31 GMT 2010
+ */
+
+
+$(function() {
+
+
+  // var lis = document.getElementById('tabs').querySelectorAll('li');
+  //
+  // for (var i = 0; i < lis.length; i++) {
+  //   lis[i].setAttribute('index', i); //给每个li设置索引号
+  //   // 第一步 导航栏lis点击变色
+  //   lis[i].onclick = function() {
+  //     console.log("my i is:" + i)
+  //     for (var i = 0; i < lis.length; i++) {
+  //       lis[i].className = '';
+  //     }
+  //     this.className = 'current';
+  //     //打印 点击li是否获取到索引号
+  //     // var index = this.getAttribute('index');
+  //     // console.log(index);
+  //     // //第二步 导航栏对应盒子里的内容
+  //     // for (var i = 0; i < items.length; i++) {
+  //     //   items[i].style.display = 'none';
+  //     // }
+  //     // items[index].style.display = 'block'; //这里不能用this this在这里指向的是lis
+  //   }
+  // }
+
+
+
+  // setTimeout(function() {
+    // 需要在这里写入您想要执行的代码
+
+    // var indicator = $('#indicator'),
+    //   indicatorHalfWidth = indicator.width()/2,
+    //   lis = $('#tabs').children('li');
+
+    // $("#tabs").tabs("#content section", {
+    //   effect: 'fade',
+    //   fadeOutSpeed: 0,
+    //   fadeInSpeed: 400,
+    //   onBeforeClick: function(event, index) {
+    //     var li = lis.eq(index);
+    //     // if (li.position() !== null){
+    //     console.log("tab li is:")
+    //     console.log(JSON.stringify(li.position()));
+    //     var newPos = li.position().left + (li.width()/2) - indicatorHalfWidth;
+    //     indicator.stop(true).animate({ left: newPos }, 600, 'easeInOutExpo');
+    //     // }
+    //   }
+    // });
+
+  // }, 1000);
+
+
+});
+
+
+
